2323d6ab04fbc183e40424f864a1b9fd2de96226
Mr3O5 <46283095+Mr3O5@users.noreply.github.com>
2021-07-20 21:28:57 +0200

    damage / collision / animation

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -11,6 +11,7 @@ class MovableObject{
     otherDirection = false;
     speedY = 0;
     acceleration = 2.5;
+    energy = 100;
 
 
     applyGravity(){
@@ -48,6 +49,33 @@ class MovableObject{
             this.imageCache[path] = img;
 
         });
+    }
+
+    draw(ctx){
+
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+    }
+
+    drawFrame(ctx){
+
+        if (this instanceof Character || this instanceof Chicken) {
+         
+        ctx.beginPath();
+        ctx.lineWidth = '5';
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+        }
+    }
+
+    isColliding(mo){
+
+        return this.x + this.width > mo.x &&
+        this.y + this.height > mo.y &&
+        this.x < mo.x &&
+        this.y < mo.y + mo.height;
+
     }
 
     playAnimation(images){
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -22,6 +22,7 @@ class World {
         this.keyboard = keyboard;
         this.draw();
         this.setWorld();
+        this.checkCollisions();
 
 
     }
@@ -30,6 +31,22 @@ class World {
 
         this.character.world = this;
 
+    }
+
+    checkCollisions(){
+
+        setInterval(() => {
+            
+            this.level.enemies.forEach((enemy) => {
+
+                if (this.character.isColliding(enemy)) {
+
+                    this.character.energy -= 5;
+                    
+                }
+                
+            })
+        }, 200);
     }
 
 
@@ -76,12 +93,17 @@ class World {
             mo.x = mo.x * -1;
         }
         
-        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        mo.draw(this.ctx);
+        mo.drawFrame(this.ctx);
+
+        
         
         if (mo.otherDirection) {
             mo.x = mo.x * -1;
             this.ctx.restore();
         }
+
+        
 
     }
 }
